6ef287fa56dbbbea201d77f1826ccfcf7c286381
Prathibha-Shivanna <prathibha55555@gmail.com>
2021-04-22 16:06:03 +0530

    adding spring boot web application

---
+++ web/src/main/java/com/example/web/WebApplication.java
@@ -0,0 +1,14 @@
+package com.example.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApplication.class, args);
+                System.out.println("hello this is web application");
+	}
+
+}
